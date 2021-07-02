b05a9830b92bbe3306145fb70ab95f1f9167d144
Milica <milicasudar@gmail.com>
2021-07-02 17:23:14 +0200

    Gitignore

---
+++ src/main/java/markovic/aleksa/njtprojekat/NjtProjekatApplication.java
@@ -0,0 +1,13 @@
+package markovic.aleksa.njtprojekat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NjtProjekatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NjtProjekatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/markovic/aleksa/njtprojekat/NjtProjekatApplicationTests.java
@@ -0,0 +1,13 @@
+package markovic.aleksa.njtprojekat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NjtProjekatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
